e2bfeb5c6919b34e075e0a07ca73456607444caf
Andrew <agcummings11@gmail.com>
2018-12-25 02:02:54 -0800

    found issue, fixed

--- create_game.php
+++ create_game.php
@@ -1,32 +1,15 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-echo "Connected. <br/>";
-
 $db = mysqli_connect("localhost", "risk_game", getenv("MYSQL_PASS"), "riskdb");
 
-$game_name = $_GET['name'];
-$game_pass = $_GET['pass'];
-$max_players = $_GET['players'];
+$game_name = $_POST['name'];
+$game_pass = $_POST['pass'];
+$max_players = $_POST['players'];
 
 if ($game_name != "") {
-	echo "Adding. <br/>";
-	echo $game_name;
-	echo "<br/>";
-	echo $game_pass;
-	echo "<br/>";
-	echo $max_players;
-	echo "<br/>";
 	$stmt = $db->prepare("INSERT INTO games (`game_name`,`game_password`,`wanted_players`) VALUES(?,?,?);");
-	$stmt->bind_param('sss', $username, $game_pass, $max_players);
+	$stmt->bind_param('sss', $game_name, $game_pass, $max_players);
 	$stmt->execute();
-	$res = $stmt->get_result();
-	echo $res;
-
-	echo "   BAM  ";
-	echo mysqli_error($db);
 }
 
 ?>
