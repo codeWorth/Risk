1a5209a542f392855d0f817438c21b4c630955bf
Andrew <agcummings11@gmail.com>
2018-12-24 19:48:31 -0800

    form and php to match

---
+++ handle_user.php
@@ -0,0 +1,49 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "Connecting to database... <br/>";
+$db = mysqli_connect("localhost", "risk_game", getenv("MYSQL_PASS"), "riskdb");
+
+
+$username = $_GET['name'];
+$password = $_GET['pass']; 
+
+if ($username != "" and $password != "") {
+	if ($_GET['new']) {
+		$stmt = $db->prepare("SELECT * FROM players WHERE `user_name`= ?");
+		$stmt->bind_param('s', $username);
+		$stmt->execute();
+		$me = $stmt->get_result();
+
+		if (mysqli_num_rows($me) != 0) {
+			echo "User already exists.";
+		} else {
+			$stmt = $db->prepare("INSERT INTO players (user_name,user_password) VALUES(?, ?)");
+			$stmt->bind_param('ss', $username, $password);
+			$stmt->execute();
+			$res = $stmt->get_result();
+
+			echo "Added user to players.";
+			echo $res;
+		}
+	} else if ($_GET['login']) {
+		$stmt = $db->prepare("SELECT `user_id` FROM players WHERE `user_name`=? AND `user_password`=?");
+		$stmt->bind_param('ss', $username, $password);
+		$stmt->execute();
+		$me = $stmt->get_result();
+
+		if (mysqli_num_rows($me) == 0) {
+			echo "User does not exist.";
+		} else {
+			echo mysqli_fetch_row($me)[0];
+		}
+	}
+} else {
+	echo "Invalid username and password (cannot be empty).";
+}
+
+
+?>
