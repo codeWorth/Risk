e6ae1c5ac2889fe108131d065ce0ebae17cbd847
Andrew <agcummings11@gmail.com>
2018-12-25 01:56:03 -0800

    even more print

--- create_game.php
+++ create_game.php
@@ -12,10 +12,18 @@ $game_pass = $_POST['pass'];
 $max_players = intval($_POST['players']);
 
 if ($game_name != "") {
-	echo "Adding.";
+	echo "Adding. <br/>";
+	echo $game_name + "<br/>";
+	echo $game_pass + "<br/>";
+	echo $max_players;
+	echo "<br/>";
 	$stmt = $db->prepare("INSERT INTO games (`game_name`,`game_password`,`wanted_players`) VALUES(?,?,?);");
 	$stmt->bind_param('ssi', $username, $game_pass, $max_players);
 	$stmt->execute();
+	$res = $stmt->get_result();
+	echo $res;
+
+	echo "   DONE";
 }
 
 ?>
