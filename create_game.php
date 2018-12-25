46538f2e9b41733488d652554f21a53624ff83d6
Andrew <agcummings11@gmail.com>
2018-12-25 01:59:57 -0800

    try to get error

--- create_game.php
+++ create_game.php
@@ -9,7 +9,7 @@ $db = mysqli_connect("localhost", "risk_game", getenv("MYSQL_PASS"), "riskdb");
 
 $game_name = $_GET['name'];
 $game_pass = $_GET['pass'];
-$max_players = intval($_GET['players']);
+$max_players = $_GET['players'];
 
 if ($game_name != "") {
 	echo "Adding. <br/>";
@@ -20,7 +20,7 @@ if ($game_name != "") {
 	echo $max_players;
 	echo "<br/>";
 	$stmt = $db->prepare("INSERT INTO games (`game_name`,`game_password`,`wanted_players`) VALUES(?,?,?);");
-	$stmt->bind_param('ssi', $username, $game_pass, $max_players);
+	$stmt->bind_param('sss', $username, $game_pass, $max_players);
 	$stmt->execute();
 	$res = $stmt->get_result();
 	echo $res;
