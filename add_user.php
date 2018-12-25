5e69c969367815234601441e7b8730a5f7c0fa53
Andrew <agcummings11@gmail.com>
2018-12-24 18:52:24 -0800

    fixed env issue, retest

--- add_user.php
+++ add_user.php
@@ -7,30 +7,29 @@ error_reporting(E_ALL);
 echo "Connecting to database... <br/>";
 $db = mysqli_connect("localhost", "risk_game", getenv("MYSQL_PASS"), "riskdb");
 
-echo "Connected to database. <br/>";
 
 $username = $_GET['name'];
 $password = $_GET['pass']; 
 
 if ($username != "" and $password != "") {
-	$stmt = $accounts->prepare("SELECT * FROM players WHERE `user_name`= ?");
+	$stmt = $db->prepare("SELECT * FROM players WHERE `user_name`= ?");
 	$stmt->bind_param('s', $username);
 	$stmt->execute();
 	$me = $stmt->get_result();
 
-	// if (mysqli_num_rows($me) != 0) {
-	// 	echo "User already exists.";
-	// } else {
-	echo "Adding username and password to users list. <br/>";
+	if (mysqli_num_rows($me) != 0) {
+		echo "User already exists.";
+	} else {
+		echo "Adding username and password to users list. <br/>";
 
-	$stmt = $accounts->prepare("INSERT INTO players (user_name,user_password) VALUES(?, ?)");
-	$stmt->bind_param('ss', $username, $password);
-	$stmt->execute();
-	$res = $stmt->get_result();
+		$stmt = $accounts->prepare("INSERT INTO players (user_name,user_password) VALUES(?, ?)");
+		$stmt->bind_param('ss', $username, $password);
+		$stmt->execute();
+		$res = $stmt->get_result();
 
-	echo "Added user to players.";
-	echo $res;
-	// }
+		echo "Added user to players.";
+		echo $res;
+	}
 } else {
 	echo "Invalid username and password (cannot be empty).";
 }
