9ca72928def0ddabe4c7e4f5c5720de0e5d5df65
Andrew <agcummings11@gmail.com>
2018-12-25 01:50:21 -0800

    game viewer

---
+++ create_game.php
@@ -0,0 +1,18 @@
+<?php
+
+$db = mysqli_connect("localhost", "risk_game", getenv("MYSQL_PASS"), "riskdb");
+
+$game_name = $_POST['name'];
+$game_pass = $_POST['pass'];
+$max_players = intval($_POST['players']);
+
+if ($game_name != "") {
+	$stmt = $db->prepare("INSERT INTO games (`game_name`,`game_password`,`wanted_players`) VALUES(?,?,?);");
+	$stmt->bind_param('ssi', $username, $game_pass, $max_players);
+	$stmt->execute();
+}
+
+header('Location: handle_user.php');
+exit();
+
+?>
---
+++ game_data.php
@@ -0,0 +1,9 @@
+<?php
+
+$db = mysqli_connect("localhost", "risk_game", getenv("MYSQL_PASS"), "riskdb");
+
+$user_id = $_GET['id'];
+
+
+
+?>
